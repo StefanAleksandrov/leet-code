7562646cdb8d37383e137e84761c2a173a69bfb6
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2022-09-01 22:44:45 +0300

    Time: 224 ms (52.86%), Space: 52.6 MB (61.43%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+	nums.sort((a,b) => a-b);
+	const result=[];
+    const limit=nums.length;
+
+	if(limit < 3) return result;
+
+	for(let i=0; i<limit; i++){
+		let left = i+1
+		let right = nums.length-1
+        if(nums[i] > 0) break;
+		if(i > 0 && nums[i] === nums[i-1]) continue;
+		while(left < right){
+			let sum = nums[i] + nums[left] + nums[right];
+
+			if(sum===0){
+				let arr=[nums[i], nums[left], nums[right]];
+				result.push(arr);
+				left++;
+				while(nums[left] === nums[left-1] && left < right) left++;
+			}else if(sum > 0){
+				right--;
+			}else{
+				left++;
+			}
+		}
+	}
+
+	return result;
+};
